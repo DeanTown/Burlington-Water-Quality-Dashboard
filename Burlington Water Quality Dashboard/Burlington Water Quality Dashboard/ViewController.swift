9df46dabba0566eb350506b737460de479b4b1dd
Prasidha Timsina <prasidha.timsina@uvm.edu>
2020-10-27 15:57:24 -0400

    setting up project and gitignore

---
+++ Burlington Water Quality Dashboard/Burlington Water Quality Dashboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Burlington Water Quality Dashboard
+//
+//  Created by Prasidha Timsina on 10/27/20.
+//  Copyright © 2020 IOTConduit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
