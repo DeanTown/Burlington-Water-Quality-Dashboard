321a2c5f73e4291264c7e84dec99923e8539fd81
Oliver Reckord Groten <oliver.reckordgroten@gmail.com>
2020-11-19 15:33:59 -0500

    create base layout for storyboard

--- BackendTest/Line_Chart_Test/Line_Chart_Test/ViewController.swift
+++ BackendTest/Line_Chart_Test/Line_Chart_Test/ViewController.swift
@@ -9,25 +9,39 @@
 import UIKit
 import Charts
 import TinyConstraints
+import SimpleCheckbox
 
 class ViewController: UIViewController, ChartViewDelegate {
     
-    lazy var lineChartView: LineChartView = {
+    
+    
+    lazy var lineChartView1: LineChartView = {
         let chartView = LineChartView()
         chartView.backgroundColor = .systemBlue
         return chartView
     }()
+    
+    lazy var lineChartView2: LineChartView = {
+           let chartView = LineChartView()
+           chartView.backgroundColor = .systemOrange
+           return chartView
+       }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.addSubview(lineChartView)
-        lineChartView.centerInSuperview()
-        lineChartView.width(to: view)
-        lineChartView.heightToWidth(of: view)
+        view.subviews[0].addSubview(lineChartView1)
+        view.subviews[1].addSubview(lineChartView2)
+        lineChartView1.centerInSuperview()
+        lineChartView1.width(to: view)
+        lineChartView1.heightToSuperview()
+        lineChartView2.centerInSuperview()
+        lineChartView2.width(to: view)
+        lineChartView2.heightToSuperview()
         
         setData()
+        
     }
 
     func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
@@ -38,7 +52,8 @@ class ViewController: UIViewController, ChartViewDelegate {
         let set1 = LineChartDataSet(entries: yValues, label: "Water Quality")
         
         let data = LineChartData(dataSet: set1)
-        lineChartView.data = data
+        lineChartView1.data = data
+        lineChartView2.data = data
     }
     
     let yValues: [ChartDataEntry] = [
