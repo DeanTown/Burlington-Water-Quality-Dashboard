9395de63ef7ebdf03472f7f38e9686ca74f74696
owenda7 <owenda7@gmail.com>
2020-11-27 22:15:31 -0500

    cyanobacteria stuff

---
+++ Burlington Water Quality Dashboard/Burlington Water Quality Dashboard/Cyanobacteria.swift
@@ -0,0 +1,9 @@
+//
+//  Cyanobacteria.swift
+//  Burlington Water Quality Dashboard
+//
+//  Created by Owen Anderson on 11/27/20.
+//  Copyright © 2020 IOTConduit. All rights reserved.
+//
+
+import Foundation
