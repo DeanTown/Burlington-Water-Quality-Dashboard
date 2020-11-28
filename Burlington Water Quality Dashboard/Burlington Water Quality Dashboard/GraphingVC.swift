f15c93647454cf24598e4d7a3aa6a11efcdea93b
Oliver Reckord Groten <oliver.reckordgroten@gmail.com>
2020-11-28 17:12:56 -0500

    add graphing screen files to main project and install pods

---
+++ Burlington Water Quality Dashboard/Burlington Water Quality Dashboard/GraphingVC.swift
@@ -0,0 +1,9 @@
+//
+//  GraphingVC.swift
+//  Burlington Water Quality Dashboard
+//
+//  Created by Oliver Reckord Groten on 11/28/20.
+//  Copyright © 2020 IOTConduit. All rights reserved.
+//
+
+import Foundation
