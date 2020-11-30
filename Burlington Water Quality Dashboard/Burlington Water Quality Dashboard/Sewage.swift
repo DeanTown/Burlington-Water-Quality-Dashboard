049e157cd54aeb2c200dd1d329ba37bec3db9ff0
Nick Hella <NickHella@macxbook.local>
2020-11-29 23:02:32 -0500

    Adding sewage files

---
+++ Burlington Water Quality Dashboard/Burlington Water Quality Dashboard/Sewage.swift
@@ -0,0 +1,98 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Nick Hella on 10/24/20.
+//
+
+import UIKit
+
+
+// Template
+class SewageDataItem: NSObject, NSCoding {
+    
+    var date: String
+    var type: String!
+    var minGal: Int!
+    var maxGal: Int!
+    let location: String!
+    let receivingWater: String!
+    let latitude: String!
+    let longitude: String!
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(date, forKey: "date")
+        aCoder.encode(type, forKey: "type")
+        aCoder.encode(minGal, forKey: "minGal")
+        aCoder.encode(maxGal, forKey: "maxGal")
+        aCoder.encode(location, forKey: "location")
+        aCoder.encode(receivingWater, forKey: "receivingWater")
+        aCoder.encode(latitude, forKey: "latitude")
+        aCoder.encode(longitude, forKey: "longitude")
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        date = aDecoder.decodeObject(forKey: "date") as! String
+        type = (aDecoder.decodeObject(forKey: "type") as! String)
+        minGal = (aDecoder.decodeObject(forKey: "minGal") as! Int)
+        maxGal = (aDecoder.decodeInteger(forKey: "maxGal") )
+        location = (aDecoder.decodeInteger(forKey: "location") as! String)
+        receivingWater = (aDecoder.decodeInteger(forKey: "receivingWater") as! String)
+        latitude = (aDecoder.decodeInteger(forKey: "latitude") as! String)
+        longitude = (aDecoder.decodeInteger(forKey: "longitude") as! String)
+
+        super.init()
+    }
+    
+    // Designated initilializer
+    init(date: String, type: String, minGal: Int, maxGal: Int, location: String, receivingWater: String, latitude: String, longitude: String) {
+        
+        self.date = date
+        self.type = type
+        self.minGal = minGal
+        self.maxGal = maxGal
+        self.location = location
+        self.receivingWater = receivingWater
+        self.latitude = latitude
+        self.longitude = longitude
+        
+        super.init()
+    }
+    
+    func toString(){
+        print("\tLocation: \(self.location ?? "")")
+        print("\t\tDate: \(self.date)")
+        print("\t\tType: \(self.type ?? "")")
+        print("\t\tMinGal: \(self.minGal ?? 0)")
+        print("\t\tMaxGal: \(self.maxGal ?? 0)")
+        print("\t\tReceivingWater: \(self.receivingWater ?? "")")
+        print("\t\tLatitude: \(self.latitude ?? "")")
+        print("\t\tLongitude: \(self.longitude ?? "")")
+    }
+    
+    func getLocation() -> String {
+        return self.location
+    }
+    func getDate() -> String  {
+        return self.date
+    }
+    func getType() -> String  {
+        return self.type
+    }
+    func getMinGal() -> Int  {
+        return self.minGal
+    }
+    func getMaxGal() -> Int  {
+        return self.maxGal
+    }
+    func getReceivingWater() -> String  {
+        return self.receivingWater
+    }
+    func getLatitude() -> String  {
+        return self.latitude
+    }
+    func getLongitude() -> String  {
+        return self.longitude
+    }
+    
+}
